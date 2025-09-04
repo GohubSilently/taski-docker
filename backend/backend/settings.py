c307a52593fdc45f2dd8e7ed714aa54976976c3a
Vadim Khalin <khalinvadim3@gmail.com>
2025-09-04 18:23:10 +0300

    Bug fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", default='localhost').split(",")
+ALLOWED_HOSTS = ['localhost', 'taskipro.gotdns.ch']
 
 
 # Application definition
